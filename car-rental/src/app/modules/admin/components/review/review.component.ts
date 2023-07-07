71d59b0c9b30b5fc71523248c85a77caa39a16c2
Akshatakallur <akshatakallur10@gmail.com>
2023-07-07 08:53:01 +0530

    final commit

--- car-rental/src/app/modules/admin/components/review/review.component.ts
+++ car-rental/src/app/modules/admin/components/review/review.component.ts
@@ -5,48 +5,48 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.css']
 })
-export class ReviewComponent implements OnInit {
-  reviews: any[] = [
-    {
-      name: 'Pooja',
-      date: 'June 15, 2023',
-      comment: 'Great car rental experience! The vehicle was clean and in excellent condition. Highly recommended.',
-      rating: 5,
-      image: '\zoomcar\car\src\assets\download.jpg'
-    },
-    {
-      name: 'Dolli',
-      date: 'June 12, 2023',
-      comment: 'Had a fantastic trip with Zoom Car. The service was prompt and the car was comfortable. Will use it again.',
-      rating: 4,
-      image: 'assets/car2.jpg'
-    },
-    {
-      name: 'kavana',
-      date: 'June 10, 2023',
-      comment: 'Average experience. The car had some minor issues, but overall it was decent. Could be better.',
-      rating: 3,
-      image: 'assets/car3.jpg'
-    },
-    {
-      name: 'akshata',
-      date: 'June 8, 2023',
-      comment: 'Disappointing service. The car was not properly cleaned and had technical problems during the trip.',
-      rating: 2,
-      image: 'assets/car4.jpg'
-    },
-    {
-      name: 'krishna',
-      date: 'June 5, 2023',
-      comment: 'Worst car rental experience ever! The vehicle was in poor condition, and the customer support was unhelpful.',
-      rating: 1,
-      image: 'assets/car5.jpg'
-    },
-    // Add more reviews as needed
-  ];
+export class ReviewComponent {
+  // reviews: any[] = [
+  //   {
+  //     name: 'Pooja',
+  //     date: 'June 15, 2023',
+  //     comment: 'Great car rental experience! The vehicle was clean and in excellent condition. Highly recommended.',
+  //     rating: 5,
+  //     image: '\zoomcar\car\src\assets\download.jpg'
+  //   },
+  //   {
+  //     name: 'Dolli',
+  //     date: 'June 12, 2023',
+  //     comment: 'Had a fantastic trip with Zoom Car. The service was prompt and the car was comfortable. Will use it again.',
+  //     rating: 4,
+  //     image: 'assets/car2.jpg'
+  //   },
+  //   {
+  //     name: 'kavana',
+  //     date: 'June 10, 2023',
+  //     comment: 'Average experience. The car had some minor issues, but overall it was decent. Could be better.',
+  //     rating: 3,
+  //     image: 'assets/car3.jpg'
+  //   },
+  //   {
+  //     name: 'akshata',
+  //     date: 'June 8, 2023',
+  //     comment: 'Disappointing service. The car was not properly cleaned and had technical problems during the trip.',
+  //     rating: 2,
+  //     image: 'assets/car4.jpg'
+  //   },
+  //   {
+  //     name: 'krishna',
+  //     date: 'June 5, 2023',
+  //     comment: 'Worst car rental experience ever! The vehicle was in poor condition, and the customer support was unhelpful.',
+  //     rating: 1,
+  //     image: 'assets/car5.jpg'
+  //   },
+  //   // Add more reviews as needed
+  // ];
 
-  constructor() { }
+  // constructor() { }
 
-  ngOnInit() {
-  }
+  // ngOnInit() {
+  // }
 }
